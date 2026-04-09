8d2d612982891a8db02b914380c987b214f76e4f
Kévin Bellaïche <kevinbellaiche@mbp-de-kevin.home>
2026-04-09 20:27:07 +0200

    Fix supabase config

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://lsydreyupczdhjhneejy.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzeWRyZXl1cGN6ZGhqaG5lZWp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NTY1NDgsImV4cCI6MjA5MTIzMjU0OH0.1wHtRLdrm8GuXv9DjP_bgPjRAscKM3kixF7p2Ow-dUE'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
